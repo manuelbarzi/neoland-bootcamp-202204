bb8fbc8515432e575033a16e6bd9b78c4912838e
jacksonbc5 <jacintblanch5@gmail.com>
2022-04-07 13:50:01 +0200

    buttons_js

--- staff/jacint-blanch/template/main.js
+++ staff/jacint-blanch/template/main.js
@@ -1,9 +1,57 @@
-var loginView = document.querySelector('.auth');
 
-const loginButton = loginView.querySelector('.auth__form__btn');
+
+// LOGIN
+
+var loginView = document.querySelector('.auth_login');
+
+var loginButton = loginView.querySelector('.auth__form__btn');
 
 loginButton.addEventListener('click', function(e) {
     console.log('hola');
     e.preventDefault()
 })
+
+// HOME LOGIN
+
+var homeView = document.querySelector('.content');
+
+var homeLoginButton = homeView.querySelector('.content__btn__login');
+
+homeLoginButton.addEventListener('click', function(e) {
+    console.log('hola');
+    e.preventDefault()
+})
+
+// HOME REGISTER
+
+var homeView = document.querySelector('.content');
+
+var homeLoginButton = homeView.querySelector('.content__btn__register');
+
+homeLoginButton.addEventListener('click', function(e) {
+    console.log('hola');
+    e.preventDefault()
+})
+
+
+
+// REGISTER
+
+var registerView = document.querySelector('.auth_register');
+
+var registerButton = registerView.querySelector('.auth__form__btn');
+
+registerButton.addEventListener('click', function(e){
+    console.log('hola');
+    e.preventDefault();
+})
+
+
+
+
+
+
+
+
+
 
