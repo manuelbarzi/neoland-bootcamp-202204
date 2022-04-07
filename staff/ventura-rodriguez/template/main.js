7578894bcc3a247b6958a3a729974a5691ca0abd
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-04-07 18:28:48 +0200

    add submit example

---
+++ staff/ventura-rodriguez/template/main.js
@@ -0,0 +1,9 @@
+var page = document.querySelector('.page')
+
+var loginForm = page.querySelector('.form')
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    debugger
+    console.log('in')
+})
