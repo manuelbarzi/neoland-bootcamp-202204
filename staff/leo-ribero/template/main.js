8feffe599f3de66aa386faca4e3d97683430902a
Leo Ribero Calvo <leoriberocalvo@MacBook-Pro-de-Leo.local>
2022-04-08 17:44:49 +0200

    friday reset, starting again

--- staff/leo-ribero/template/main.js
+++ staff/leo-ribero/template/main.js
@@ -1,15 +1,43 @@
 var landingView = document.querySelector('.landing')
 var loginView = document.querySelector('.login')
 var registerView = document.querySelector('.register')
+var homeView = document.querySelector('.home')
+// ********************************* JS LANDING  porque landingView.querySelector
+// Estando en landingView, al ir a LOGIN    apago la vista incial landigView y enciendo loginView
+// Estando en landingView, al ir a REGISTER apago la vista incial landigView y enciendo registerView
+var landingLoginAnchor = landingView.querySelector('.login-anchor')
+landingLoginAnchor.addEventListener('click', function(){
+	landingView.classList.add('off')
+	loginView.classList.remove('off')
+    // apago landigView y enciendo loginView
+})
+var landingRegisterAnchor = landingView.querySelector('.register-anchor')
+landingRegisterAnchor.addEventListener('click', function(){
+    landingView.classList.add('off')
+    registerView.classList.remove('off')
+    // apago landigView y enciendo registerView
+})
+// ********************************* JS LOGIN  porque loginView.querySelector
+// Estando en loginView, al ir a LOGIN ahora es un botón que hace submit
+// Estando en loginView, al ir a REGISTER apago la vista loginView y enciendo registerView
+var loginRegisterAnchor = loginView.querySelector('.register-anchor')
+loginRegisterAnchor.addEventListener('click', function(){
+    loginView.classList.add('off')
+    registerView.classList.remove('off')
+})
+// ********************************* JS REGISTER  porque registerView.querySelector
+// Estando en registerView, al ir a LOGIN    apago la vista registerView y enciendo loginView
+// Estando en registerView, al ir a REGISTER ahora es un botón que hace submit
+var registerLoginAnchor = registerView.querySelector('.login-anchor')
+registerLoginAnchor.addEventListener('click', function(){
+    registerView.classList.add('off')
+    loginView.classList.remove('off')
+} )
+
+
 
 landingView.classList.remove('off')
+// loginView.classList.remove('off')
+// registerView.classList.remove('off')
+// homeView.classList.remove('off')
 
-var landingLoginButton = landingView.querySelector('.login-button')
-
-landingLoginButton.addEventListener('click', function() {
-    landingView.classList.add('off')
-
-    loginView.classList.remove('off')
-})
-
-// TODO mechanise all other navigations
