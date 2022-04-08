c9b225c864b68e743323c359cea187a3b5138db5
manuelbarzi <manuelbarzi@gmail.com>
2022-04-08 11:48:59 +0200

    add more behavior to template

--- staff/manuel-barzi/template/main.js
+++ staff/manuel-barzi/template/main.js
@@ -1,22 +1,22 @@
 var landingView = document.querySelector('.landing')
 var loginView = document.querySelector('.login')
 var registerView = document.querySelector('.register')
+var homeView = document.querySelector('.home')
 
-landingView.classList.remove('off')
+//landingView.classList.remove('off')
+homeView.classList.remove('off')
 
 var landingLoginAnchor = landingView.querySelector('.login-anchor')
 
-landingLoginAnchor.addEventListener('click', function() {
+landingLoginAnchor.addEventListener('click', function () {
     landingView.classList.add('off')
 
     loginView.classList.remove('off')
 })
 
-// TODO mechanise all other navigations
-
 var landingRegisterAnchor = landingView.querySelector('.register-anchor')
 
-landingRegisterAnchor.onclick = function() {
+landingRegisterAnchor.onclick = function () {
     landingView.classList.add('off')
 
     registerView.classList.remove('off')
@@ -24,8 +24,32 @@ landingRegisterAnchor.onclick = function() {
 
 var loginRegisterAnchor = loginView.querySelector('.register-anchor')
 
-loginRegisterAnchor.onclick = function() {
+loginRegisterAnchor.onclick = function () {
     loginView.classList.add('off')
 
     registerView.classList.remove('off')
 }
+
+// TODO make navigation from register to login
+
+var loginForm = loginView.querySelector('form')
+
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var usernameInput = loginForm.querySelector('input[name=username]')
+    var passwordInput = loginForm.querySelector('input[name=password]')
+
+    var username = usernameInput.value
+    var password = passwordInput.value
+
+    if (username !== 'pepito' || password !== '123123123') {
+        alert('wrong credentials')
+
+        return
+    }
+
+    loginView.classList.add('off')
+
+    homeView.classList.remove('off')
+})
