81943d23c218e22d30ecb50dc3f7dd4aac0de62a
JSchaefer21 <jschaefer21@gmail.com>
2022-04-08 14:00:26 +0200

    Update HTML, CSS and JAVA files

--- staff/jordi-schaefer/template/main.js
+++ staff/jordi-schaefer/template/main.js
@@ -1,9 +1,13 @@
 var landingView = document.querySelector('.landing')
 var loginView = document.querySelector('.login')
 var registerView = document.querySelector('.register')
+var homeView = document.querySelector('.home')
 
 landingView.classList.remove('off')
+//homeView.classList.remove('off')
 
+
+//     -- Movimiento inicial entre paginas --
 var LandingRegisterButton = landingView.querySelector('.start-button')
 var LandingLoginButton = landingView.querySelector('.have-button')
 var loginBackButton = loginView.querySelector('.back-button')
@@ -17,7 +21,6 @@ LandingRegisterButton.addEventListener('click', function() {
     landingView.classList.add('off')
     registerView.classList.remove('off')
 })
-
 loginBackButton.addEventListener('click', function() {
     loginView.classList.add('off')
     landingView.classList.remove('off')
@@ -28,8 +31,34 @@ registerBackButton.addEventListener('click', function() {
 })
 
 
+// si en la consola escribes una variable, te deberia mostrar lo que selecciona
 
-var loginForm = document.querySelector('form')
-loginForm.addEventListener('submit', function(){
+//     -- funciones de login y Registro--
+var loginForm = loginView.querySelector('form')
+var registerForm = registerView.querySelector('form')
+loginForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    //alert("login!")
+    //debugger // se utiliza para hacer un brake en javascript
+ /*   
+    var usernameInput = loginForm.querySelector('input[name=username]')
+    var passwordInput = loginForm.querySelector('input[name=password]')
     
+    console.log(usernameInput.value, passwordInput.value)
+
+    if (username !== 'user' || password !== '1234')
+        alert('user not found')
+    else {
+        loginView.classList.add('off')
+        homeView.classList.remove('off')
+    }
+*/
+    loginView.classList.add('off')
+    homeView.classList.remove('off')
+})
+registerForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    //alert("register!")
+    registerView.classList.add('off')
+    homeView.classList.remove('off')
 })
