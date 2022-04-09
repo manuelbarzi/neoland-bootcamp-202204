0cd54f11db4bd4d9f449a1b4d194e2be3fd01d84
Diegocchaves <diegocchaves21@gmail.com>
2022-04-09 20:13:59 +0200

    Co-authored-by: JSchaefer21 <JSchaefer21@users.noreply.github.com>

--- staff/diego-carvalho/template/main.js
+++ staff/diego-carvalho/template/main.js
@@ -1,26 +1,78 @@
-var landingView = document.querySelector('.landing')
-var registerView = document.querySelector('.register')
-var loginView = document.querySelector('.login')
+var landingView = document.querySelector('.landing-page')
+var loginView = document.querySelector('.login-page')
+var registerView = document.querySelector('.register-page')
+var homeView = document.querySelector('.home-page')
 
 landingView.classList.remove('off')
-var landingLoginButton = landingView.querySelector('.login-button')
+var landingLoginButton = landingView.querySelector('.signin-button')
 
 landingLoginButton.addEventListener('click', function() {
     landingView.classList.add('off')
+
     loginView.classList.remove('off')
 })
 
-var landingRegisterButton = landingView.querySelector('.register-button')
+var landingRegisterButton = landingView.querySelector('.signup-button')
 
-landingRegisterButton.addEventListener('click', function() {
+landingRegisterButton.onclick = function() {
     landingView.classList.add('off')
+
     registerView.classList.remove('off')
+}
+
+
+//TODO make navigation from login to home
+
+var loginForm = loginView.querySelector('form')
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var emailInput = loginView.querySelector('input[name=email]')
+    var passwordInput = loginView.querySelector('input[name=password]')
+
+    var nameEmail = emailInput.value
+    var namePassword = passwordInput.value
+
+    if (nameEmail !== 'diego@gmail.com' || namePassword !== '1234') {
+        alert('wrong credentials')
+
+        return
+    }
+
+    loginView.classList.add('off')
+
+    homeView.classList.remove('off') 
+
 })
 
 
+var registerForm = registerView.querySelector('form')
 
+registerForm.addEventListener('submit', function (event) {
 
+    event.preventDefault()
 
+    var userInput = registerForm.querySelector('input[name=username]')
+    var mailInput = registerForm.querySelector('input[name=email]')
+    var passInput = registerView.querySelector('input[name=password]')
+    var rpasswordInput = registerForm.querySelector('input[name=r-password]')
+
+    var usuario = userInput.value
+    var mail = mailInput.value
+    var contraseña = passInput.value
+    var rcontraseña = rpasswordInput.value
+
+        if (usuario !== 'diego'|| mail !== 'diego@gmail.com' || contraseña !== '1234' || rcontraseña !== '1234')
+
+        return
+
+    registerView.classList.add('off')
+
+    loginView.classList.remove('off')
+
+})
+
+//TODO make navegation from register to home
 
 
 
