8a89487a1c580753937755b61a99b7e5b0202ef4
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-04-10 10:48:53 +0200

    grid completed

--- staff/federico-lopez/template/main.js
+++ staff/federico-lopez/template/main.js
@@ -1,41 +1,41 @@
 var landingView = document.querySelector('.landing-view')
-var signUpView = document.querySelector('.sign-up-view')
+var registerView = document.querySelector('.sign-up-view')
 var logInView = document.querySelector('.log-in-view')
 var homeView = document.querySelector('.home')
 
 landingView.classList.remove('off')
 // homeView.classList.remove('off')
 
-var bottonSignUpLandingView = document.querySelector('.button-sign-up-landing-view')
-var bottonLogInLandingView = document.querySelector('.botton-log-in-landing-view')
+var landingRegisterButton = document.querySelector('.button-sign-up-landing-view')
+var landingLogInButton = document.querySelector('.botton-log-in-landing-view')
 
 
-bottonSignUpLandingView.addEventListener('click', function() {
+landingRegisterButton.addEventListener('click', function() {
     landingView.classList.add('off')
-    signUpView.classList.remove('off')})
+    registerView.classList.remove('off')})
 
-var bottonRegistered = document.querySelector('.button-already-registered')
+var registerAlreadyRegisteredButton = document.querySelector('.button-already-registered')
 
-bottonRegistered.addEventListener('click', function() {
-    signUpView.classList.add('off')
+registerAlreadyRegisteredButton.addEventListener('click', function() {
+    registerView.classList.add('off')
     logInView.classList.remove('off')
 })
 
-bottonLogInLandingView.addEventListener('click', function(){
+landingLogInButton.addEventListener('click', function(){
     landingView.classList.add('off')
     logInView.classList.remove('off')
 })
 
-var bottonNotRegistered = document.querySelector('.button-not-registered')
+var loginNotRegisteredButton = document.querySelector('.button-not-registered')
 
-bottonNotRegistered.addEventListener('click', function(){
+loginNotRegisteredButton.addEventListener('click', function(){
     logInView.classList.add('off')
-    signUpView.classList.remove('off')
+    registerView.classList.remove('off')
 })
 
-var formLogInView = logInView.querySelector('form')
+var loginForm = logInView.querySelector('form')
 
-formLogInView.addEventListener('submit', function(event){
+loginForm.addEventListener('submit', function(event){
     event.preventDefault()
     var emailUser = event.target.email.value
     var passwordUser = event.target.password.value
@@ -43,10 +43,10 @@ formLogInView.addEventListener('submit', function(event){
     homeView.classList.remove('off')
 })
 
-var formSignUpView = signUpView.querySelector('form')
+var registerForm = registerView.querySelector('form')
 
-formSignUpView.addEventListener('submit', function(event) {
+registerForm.addEventListener('submit', function(event) {
     event.preventDefault()
-    signUpView.classList.add('off')
+    registerView.classList.add('off')
     logInView.classList.remove('off')
 })
