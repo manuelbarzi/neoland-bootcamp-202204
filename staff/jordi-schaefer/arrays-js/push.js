d370ae06f04e10154182d8cdbe041320524a1a11
JSchaefer21 <jschaefer21@gmail.com>
2022-04-14 15:41:07 +0200

    Update JS exercises files

---
+++ staff/jordi-schaefer/arrays-js/push.js
@@ -0,0 +1,28 @@
+
+function push(array, element){
+    array[array.length]=element
+    return array.length
+}
+
+/*
+IMPORTANTE !
+
+aun teniendo dentro de la funcion push otros nombres, dentro de la funcion siempre hacen referencia al original y modifical el original 
+la referencia ha referencia ( o apunta ) en los dos casos a la misma memoria
+para crear uno nuevo no puedo crearlo sin mas
+¡¡ NO !!  new_array=array ¡¡ NO !!
+de este modo estoy haciendo una copia de la direccion
+
+He de generar uno nuevo y pasarle/copiarle los valores de uno a uno
+
+
+function push(array, element){
+    const new_array = []
+    for (let i = 0; i < array.length; i++){
+        new_array [i]=array[i]
+    }
+    new_array[new_array.length]=element
+    return new_array.length
+}
+*/
+
