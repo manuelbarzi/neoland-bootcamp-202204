ab69b7881ff326d76c7b5b4edfe10f5a35425be3
JSchaefer21 <jschaefer21@gmail.com>
2022-04-17 21:52:37 +0200

    Update JS exercise files

--- staff/jordi-schaefer/arrays-js/sort.js
+++ staff/jordi-schaefer/arrays-js/sort.js
@@ -1,6 +1,37 @@
 
 function sort (array){
 
+
+    let memoria = 0
+    let n = 0
+
+    for (let i = 0; i < array.length; i++){  // por cada elemento del array
+        let indice = i                          // me guardo el indice actual
+        for ( let j = i+1; j < array.length; j++){ // recorro todo el restante del array
+
+            while (String(array[j])[n] = String(array[indice])[n]){        // si el primero son iguales, y mientras los siguientes sean iguales
+                if (String(array[j])[n+1] < String(array[indice])[n+1]){   // comprueba los siguientes valores y cuando uno sea menos guarda el indice    
+                    indice = j                    
+                }   
+                n++                
+            }
+            n=0
+
+
+            if (String(array[j])[0] < String(array[indice])[0]){         // buscando el mas pequeño de todos los restantes
+                indice = j                    //  guardo el indice
+            }
+        }     
+        
+        
+        memoria = array[i]        // y los intercambio
+        array[i] = array[indice]
+        array[indice] = memoria
+    }
+
+
+
+
 /*   
     // SELECTION SORT
     let memoria = 0
@@ -16,9 +47,9 @@ function sort (array){
         array[i] = array[indice]
         array[indice] = memoria
     }
-*/
 
 
+   
     // BUBBLE SORT
     let memoria = 0
 
@@ -36,4 +67,7 @@ function sort (array){
     // MERGE SORT
     // el mas optimizado pero mas largo de escribir
     // separo todos en arbol de forma individual, y los voy unicoon ordenado, primero unitario, luego dos y dos, luego cuatro y cuatro...
+
+*/
+
 }
--- staff/jordi-schaefer/arrays-js/sort.test.js
+++ staff/jordi-schaefer/arrays-js/sort.test.js
@@ -1,7 +1,87 @@
 {
     console.log('TEST sort')
 
+
+    {
+        console.log('CASE: 1')
+        const numbers = [4, 3, 7, 1, 5, 6, 2, 10, 9, 8]
+        // expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        
+        sort(numbers) // ordenar el array
+        console.assert(numbers.length === 10) // el array debe ser igual de largo
+        console.assert(numbers[0] === 1) 
+        console.assert(numbers[1] === 10)
+        console.assert(numbers[2] === 2) 
+        console.assert(numbers[3] === 3)
+        console.assert(numbers[4] === 4)
+        console.assert(numbers[5] === 5)
+        console.assert(numbers[6] === 6)
+        console.assert(numbers[7] === 7)
+        console.assert(numbers[8] === 8)
+        console.assert(numbers[9] === 9)
+    }
+
     
+    {
+        console.log('CASE: 2')
+        let stringArray = ['Bbbblue', 'Humpback', 'Bbbblue', 'Bbbbeluga'];
+        // ['Beluga', 'Blue', 'Humpback']
+
+        sort(stringArray)
+
+        console.assert(stringArray[0] === 'Bbbbeluga') 
+        console.assert(stringArray[1] === 'Bbbblue')
+        console.assert(stringArray[2] === 'Bbbblue')
+        console.assert(stringArray[3] === 'Humpback')
+    }
+
+
+    {
+        console.log('CASE: 3')
+        let numberArray = [40, 1, 5, 200];
+        // [1, 200, 40, 5]
+
+        sort(numberArray)
+
+        console.assert(numberArray[0] === 1) 
+        console.assert(numberArray[1] === 200)
+        console.assert(numberArray[2] === 40)
+        console.assert(numberArray[3] === 5)
+    }
+
+
+    {
+        console.log('CASE: 4')
+        let numericStringArray = ['80', '9', '700'];
+        // ['700', '80', '9']
+
+        sort(numericStringArray)
+
+        console.assert(numericStringArray[0] === '700') 
+        console.assert(numericStringArray[1] === '80')
+        console.assert(numericStringArray[2] === '9')
+    }
+
+
+    {
+        console.log('CASE: 5')
+        let mixedNumericArray = ['80', '9', '700', 40, 1, 5, 200];
+        // [1, 200, 40, 5, '700', '80', '9']
+
+        sort(mixedNumericArray)
+
+        console.assert(mixedNumericArray[0] === 1) 
+        console.assert(mixedNumericArray[1] === 200)
+        console.assert(mixedNumericArray[2] === 40)
+        console.assert(mixedNumericArray[3] === 5) 
+        console.assert(mixedNumericArray[4] === '700')
+        console.assert(mixedNumericArray[5] === '80')
+        console.assert(mixedNumericArray[6] === '9')
+    }
+
+    
+
+ /*      Test para un sort normal   
     {
         console.log('CASE: 1')
         const numbers = [4, 3, 7, 1, 5, 6, 2, 10, 9, 8]
@@ -39,7 +119,9 @@
         console.assert(numbers[7] === 4)
         console.assert(numbers[8] === 4)
         console.assert(numbers[9] === 5)
-    }
 
+        console.log(numbers)
+    }
+*/
 
 }
