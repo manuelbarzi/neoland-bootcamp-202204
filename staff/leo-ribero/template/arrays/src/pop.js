ff8221766565986e276253b45342a2bec5897ee2
Leo Ribero Calvo <leoriberocalvo@MacBook-Pro-de-Leo.local>
2022-04-18 19:46:23 +0200

    adding push solution

--- staff/leo-ribero/template/arrays/src/pop.js
+++ staff/leo-ribero/template/arrays/src/pop.js
@@ -1,13 +1,21 @@
-/*
 
-VERSION caso 1
 
-*/
+// function pop(array) {
+//     const newArrayFormerLast = array[array.length-1]
+//     const formerArrayLength = array.length-1
+//     array.length = formerArrayLength
+//     return array
+// }
 
-function pop(array) {
-    const newArrayFormerLast = array[array.length-1]
-    const formerArrayLength = array.length-1
-    array.length = formerArrayLength
-    return array
+function pop(array){
+    if (array.length === 0) {
+        return undefined
+    }
+    else {
+        const result = array[array.length-1]
+        array.length = array.length-1
+        return result
+    }
+  
+    
 }
-
--- staff/leo-ribero/template/arrays/src/pop.test.js
+++ staff/leo-ribero/template/arrays/src/pop.test.js
@@ -4,12 +4,32 @@
     {
         console.log('CASE 1')
 
-        const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
-        
+        const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato']
         const result = pop(plants)
 
         console.assert(result === 'tomato')
         console.assert(plants.length === 4)
 
     }
+    {
+        console.log('CASE 2')
+
+        const vegetables = []
+        const result = pop(vegetables)
+
+        console.assert(result === undefined)
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
