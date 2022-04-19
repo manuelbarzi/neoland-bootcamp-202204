3a083a5702e6ab123675bcd5a1522810a2e42757
manuelbarzi <manuelbarzi@gmail.com>
2022-04-19 13:13:12 +0200

    add closures, constructor functions, method-chaining, prototyping...

---
+++ stuff/js/objects.js
@@ -0,0 +1,23 @@
+o = {}
+// {}
+o.name = 'O'
+// 'O'
+o.salute = function(to) {
+    return 'hello ' + to
+}
+// ƒ (to) {
+//     return 'hello ' + to
+// }
+// o
+// {name: 'O', salute: ƒ}
+o.salute('P')
+// 'hello P'
+o = {
+    name: 'O',
+    salute(to) {
+        return 'hello ' + to
+    }
+}
+// {name: 'O', salute: ƒ}
+o.salute('P')
+// 'hello P'
