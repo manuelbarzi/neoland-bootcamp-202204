0b0f0ccb9cdb4a225a550c66f8d776d97472e6da
JSchaefer21 <jschaefer21@gmail.com>
2022-04-24 17:02:07 +0200

    Update JS files

--- staff/jordi-schaefer/js-files/fake-arrays/src/Fakay.prototype.sort.js
+++ staff/jordi-schaefer/js-files/fake-arrays/src/Fakay.prototype.sort.js
@@ -1,42 +1,23 @@
 
 Fakay.prototype.sort = function() {
 
-    for (let i = 0; i < this.length; i++){
-        
+    let indice
+    let current
+    for (let i = 0; i < this.length-1; i++){
+        indice = i+1
+        for (let j = i+1; j < this.length; j++){
+            if (this[j].toString()<this[i].toString() && this[j].toString()<this[indice].toString()){
+                indice = j
+            }
+        }
+        if (this[indice].toString()<this[i].toString()){
+            current = this[i]
+            this[i] = this[indice]
+            this[indice] = current
+        }
     }
 
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
