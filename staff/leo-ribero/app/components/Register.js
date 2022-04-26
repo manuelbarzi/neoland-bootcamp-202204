e65cc9cec9656eaac5234dd64ec2905cb5d8c81d
Leo Ribero Calvo <leoriberocalvo@MacBook-Pro-de-Leo.local>
2022-04-26 18:12:43 +0200

    adding tuesday work

---
+++ staff/leo-ribero/app/components/Register.js
@@ -0,0 +1,59 @@
+// function Register() {
+//     Component.call(this, `<div class="Register">
+//         <form class="Register__form">
+//             <input type="text" name="name" placeholder="name">
+//             <input type="text" name="username" placeholder="username">
+//             <input type="password" name="password" placeholder="password">
+//             <button>Register</button>
+//             <a href="#">Login</a>
+//         </form>
+//     </div>`)
+// }
+
+
+/*
+aquí creo la p´gina Register mediante una función
+que construye el html mediante .call
+*/
+function Register(){
+    Component.call(this, `<div class="Register">
+            <form class="Register__form">
+                <input type="text" name="name" placeholder="name">
+                <input type="text" name="username" placeholder="username">
+                <input type="password" name="password" placeholder="password">
+                <button>Register</button>
+                <a href="#">Login</a>
+            </form>
+        </div>`)
+}
+
+// chainPrototypes(Component, Register)
+chainPrototypes(Component, Register)
+
+// Register.prototype.onSubmit = function(callback) {
+//     const form = this.container.querySelector('form')
+
+//     form.addEventListener('submit', function(event) {
+//         event.preventDefault()
+
+//         const name = form.name.value
+//         const username = form.username.value
+//         const password = form.password.value
+
+//         callback(name, username, password)
+//     })
+// }
+Register.prototype.onSubmit = function(callback){
+    const form = this.container.querySelector('form')
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+         const name = form.name.value
+         const username = form.username.value
+         const password = form.password.value
+
+         callback(name, username, password)
+    })
+}
+
+// TODO implement onLoginClick method (SEE Login.prototype.onRegisterClick for inspiration)
---
+++ staff/leo-ribero/app/index.js
@@ -0,0 +1,31 @@
+/* 001
+const root = document.querySelector('#root')
+
+const register = new Register
+
+root.append(register.container)
+*/
+// 001^son los primeros pasos. Luego el root.append quedará abajo al final ya que entre medio construiremos la libreria de Funciones constructoras
+
+const root = document.querySelector('#root')
+
+const register = new Register
+//^ aquí creo una constante con un nuevo Register, la función que esta en Register.js
+
+register.onSubmit(function(name, username, password){
+    const user = { 
+                name: name, 
+                username: username, 
+                password: password 
+            }
+        users.push(user) 
+        // añadimos un objeto con tres valores en el array
+        // root.removeChild(register.container) //esta claro, quitamos register. ¿porque container? pues porque en la linea 8 de smart-components.js le dijimos que "container", con "this.container"
+        // root.appendChild(login.container)
+
+        root.removeChild(register.container)
+        root.appendChild(login.container)    
+})
+
+
+root.append(register.container)
