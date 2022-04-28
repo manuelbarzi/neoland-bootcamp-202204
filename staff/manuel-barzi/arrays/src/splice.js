c09df5fac3b4088bdc866f25a8b4ec488c7c38bb
manuelbarzi <manuelbarzi@gmail.com>
2022-04-28 10:36:35 +0200

    add more test cases for splice (arrays, by fede)

--- staff/manuel-barzi/arrays/src/splice.js
+++ staff/manuel-barzi/arrays/src/splice.js
@@ -1,15 +1,37 @@
-function splice(array, index, deleteCount, newElement) {
+function splice(array, index, deleteCount, ...newElements) {
     const deletedElements = []
 
     if (deleteCount === 1) {
         deletedElements[0] = array[index]
 
-        array[index] = newElement
-    } else {
+        for (let i = index; i < array.length - 1; i++)
+            array[i] = array[i + 1]
+
+        // array.length = array.length - 1
+        // array.length -= 1
+        array.length--       
+    } else if (deleteCount > 1) {
+        for (let i = 0; i < deleteCount; i++)
+            deletedElements[i] = array[index + i]
+
+        for (let i = index; i < array.length - deleteCount; i++)
+            array[i] = array[i + deleteCount]
+
+        // array.length = array.length - deleteCount
+        array.length -= deleteCount
+    }
+    
+    if (newElements.length === 1) {
         for (let i = array.length - 1; i >= index; i--)
             array[i + 1] = array[i]
 
-        array[index] = newElement
+        array[index] = newElements[0]
+    } else if (newElements.length > 1) {
+        for (let i = array.length - 1; i >= index; i--)
+            array[i + newElements.length] = array[i]
+        
+        for (let i = 0; i < newElements.length; i++)
+            array[index + i] = newElements[i]
     }
 
     return deletedElements
--- staff/manuel-barzi/arrays/src/splice.test.js
+++ staff/manuel-barzi/arrays/src/splice.test.js
@@ -17,11 +17,51 @@ describe('splice', () => {
 
         const deletedElements = splice(months, 4, 1, 'May')
 
-        const expectedMonths = ["Jan", "Feb", "March", "April", "May"]
+        const expectedMonths = ['Jan', 'Feb', 'March', 'April', 'May']
 
         const expectedDeletedElements = ['June']
 
         expect(months).toEqual(expectedMonths)
         expect(deletedElements).toEqual(expectedDeletedElements)
     })
+
+    test('inserts drum and guitar from index 2', () => {
+        const myFish = ['angel', 'clown', 'mandarin', 'sturgeon']
+
+        const deletedElements = splice(myFish, 2, 0, 'drum', 'guitar')
+
+        const expectedMyFish = ['angel', 'clown', 'drum', 'guitar', 'mandarin', 'sturgeon']
+
+        const expectedDeletedElements = []
+
+        expect(myFish).toEqual(expectedMyFish)
+        expect(deletedElements).toEqual(expectedDeletedElements)
+    })
+
+    test('delete one element -mandarin- from index 3', () => {
+        const myFishAndOthers = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon', 'shark', 'dolphin']
+
+        const deletedElements = splice(myFishAndOthers, 3, 1)
+        
+        const expectedMyFishAndOthers = ['angel', 'clown', 'drum', 'sturgeon', 'shark', 'dolphin']
+        
+        const expectedDeletedElements = ['mandarin']
+
+        expect(myFishAndOthers).toEqual(expectedMyFishAndOthers)
+        expect(deletedElements).toEqual(expectedDeletedElements)
+    })
+
+    test('from index 0, delete 2 elements -angel and clown- and insert 3 elements -parrot, anemone and blue', () => {
+        const myFish = ['angel', 'clown', 'trumpet', 'sturgeon']
+
+        const deletedElements = splice(myFish, 1, 2, 'parrot', 'anemone', 'blue')
+
+        const expectedMyFish = ['angel', 'parrot', 'anemone', 'blue', 'sturgeon']
+
+        const expectedDeletedElements = ['clown', 'trumpet']
+
+        expect(myFish).toEqual(expectedMyFish)
+        expect(deletedElements).toEqual(expectedDeletedElements)
+    })
+
 })
