954bb3fd3b2f0a0f10fcbd8d8d73d011daf1f7dc
leoribero <leo@ribero.net>
2022-04-28 15:54:58 +0200

    adding starting point news-app in order to start to create sticker-app step by step

---
+++ staff/leo-ribero/news-app.1/data.js
@@ -0,0 +1,41 @@
+const users = [
+	{
+		name: 'Pepito Grillo',
+		username: 'pepito',
+		password: '123123123'
+	},
+	{
+		name: 'Peter Pan',
+		username: 'peter',
+		password: '123123123'
+	},
+	{
+		name: 'Wendy Bread',
+		username: 'wendy',
+		password: '123123123'
+	}
+]
+
+const newspapers = [
+	{
+		name: 'El Diario',
+		url: 'https://eldiario.es'
+	},
+	{
+		name: 'El Mundo',
+		url: 'https://elmundo.es'
+	},
+	{
+		name: 'La Vanguardia',
+		url: 'https://lavanguardia.com'
+	},
+	{
+		name: 'El Pais',
+		url: 'https://elpais.com'
+	},
+	{
+		name: 'El Periódico',
+		url: 'https://elperiodico.com'
+	}
+	// TODO add more news sources
+]
---
+++ staff/leo-ribero/news-app.1/utils.js
@@ -0,0 +1,4 @@
+function chainPrototypes(parent, child) {
+	child.prototype = Object.create(parent.prototype)
+	child.prototype.constructor = child
+}
