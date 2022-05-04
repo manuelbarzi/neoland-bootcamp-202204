1bb833a0c84e7a9a161daf56d351b3045b2f960c
Diegocchaves <diegocchaves21@gmail.com>
2022-05-04 21:02:51 +0200

    add deleteUser file

---
+++ staff/diego-carvalho/sticker-app-class/src/components/DeleteUser.js
@@ -0,0 +1,13 @@
+class DeleteUser {
+    constructor() {
+        super(`<div>
+        <form>
+            <input class="" type="password" name="password" placeholder="password"></input>
+
+            <button>Delete User</button>
+        </form>
+    </div>`)
+    
+
+    this.container.querySelector()
+
