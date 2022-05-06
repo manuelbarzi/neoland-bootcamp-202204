7f3e9826e762006b7bce17f160a2863a52e75719
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2022-05-06 16:32:07 +0200

    add more staff

--- staff/ventura-rodriguez/calculin.1/src/components/Result.jsx
+++ staff/ventura-rodriguez/calculin.1/src/components/Result.jsx
@@ -1,7 +1,16 @@
-const { useState } = React
+const { useState, useEffect } = React
 
 
-function Result({value}) {
-    const [result, setResult] = useState(value)
-    return <div className="Result">{result}</div>
+function Result(props) {
+
+    const { value } = props
+
+    const [state, setState] = useState(value)
+    
+    useEffect(() => {
+        debugger
+        setState(value)
+    }, [value])
+
+    return <div className="Result">{state}</div>
 }
