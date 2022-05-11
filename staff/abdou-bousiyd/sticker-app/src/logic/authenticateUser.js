868794c5e337e28193857d50d93eb5eb637757ac
abousiyd <abdou.bousiyd@gmail.com>
2022-05-11 23:53:38 +0200

    add Apium to the logics

--- staff/abdou-bousiyd/sticker-app/src/logic/authenticateUser.js
+++ staff/abdou-bousiyd/sticker-app/src/logic/authenticateUser.js
@@ -1,33 +1,30 @@
+
+
+
 function authenticateUser(username, password, callback) {
-    
-    const xhr = new XMLHttpRequest
-    const url = 'https://b00tc4mp.herokuapp.com/api/v2/users/auth'
+    const api = new Apium
 
-    xhr.addEventListener('load', event => {
-        const status = event.target.status
+    api.call('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users/auth', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    }, (error, { status, payload }) => {
+        if (error) {
+            callback(error)
+            return
+        }
 
-        if(status === 200) {
-            const json = event.target.responseText
-
-            const data = JSON.parse(json)
+        if (status === 200) {
+            const data = JSON.parse(payload)
 
             callback(null, data.token)
-        }else if (status >= 400 && status < 500) {
-            const json = event.target.responseText
-
-            const data = JSON.parse(json)
+        } else if (status >= 400 && status < 500) {
+            const data = JSON.parse(payload)
 
             callback(new Error(data.error))
-        } else callback(new Error('server error'))
+        } else {
+            callback(new Error('server error'))
+        }
     })
-
-    xhr.open('POST', url)
-
-    xhr.setRequestHeader('Content-Type', 'application/json')
-
-    const data = { username, password}
-
-    const json = JSON.stringify(data)
-
-    xhr.send(json)
 }
--- staff/abdou-bousiyd/sticker-app/src/logic/deleteNote.js
+++ staff/abdou-bousiyd/sticker-app/src/logic/deleteNote.js
@@ -1,68 +1,126 @@
 function deleteNote(token, noteId, callback) {
 
-    const xhr = new XMLHttpRequest
+    const url = 'https://b00tc4mp.herokuapp.com/api/v2/users'
 
-    xhr.addEventListener('load', event => {
+    const api = new Apium
 
-        const status = event.target.status
-
-        
-        if (status === 200) {
-            
-            const json = event.target.responseText
-            const data = JSON.parse(json)
-
+    api.call('GET', url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ token, noteId })
+    }, (error, {status, payload}) => {
+        if(error) {
+            callback(error)
+            return
+        }
+        if(status === 200) {
+            const data = JSON.parse(payload) 
             const { notes = [] } = data
-            // const notes = data.notes
-            
+
             if (noteId) {
                 
                 const index = notes.findIndex(note => note.id === noteId)
                 notes.splice(index, 1)
-
-                {
-                    const xhr = new XMLHttpRequest
-    
-                    xhr.addEventListener('load', event => {
-    
-                        const status = event.target.status
-    
-                        if (status === 204) {
-                            callback(null)
-                        } else if (status >= 400 && status < 500) {
-                            const json = event.target.responseText
-    
-                            const data = JSON.parse(json)
-    
-                            callback(new Error(data.error))
-                        } else callback(new Error('server error'))
-                    })
-    
-                    xhr.open('PATCH', 'https://b00tc4mp.herokuapp.com/api/v2/users')
-    
-                    xhr.setRequestHeader('Authorization', `Bearer ${token}`)
-                    xhr.setRequestHeader('Content-Type', 'application/json')
-    
-                    const data = { notes }
-    
-                    const json = JSON.stringify(data)
-    
-                    xhr.send(json)
-    
-                }
             }
-        } else if (status >= 400 && status < 500) {
-            const json = event.target.responseText
+            api.call('PATCH', url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({ token, notes })
+            }, (error, {status, payload}) => {
+                if (error) {
+                    callback(error)
+                    return
+                }
+                if(status === 204) {
+                    callback(null)
+                }else if (status >= 400 && status < 500) {
+        
+                    const data = JSON.parse(payload)
+        
+                    callback(new Error(data.error))
+                } else {
+        
+                    callback(new Error('server error'))
+                }
+            })
+            callback(null, data.token)
 
-            const data = JSON.parse(json)
-
+        }else if (status >= 400 && status < 500) {
+            const data = JSON.parse(payload)
             callback(new Error(data.error))
-        } else callback(new Error('server error'))
+        } else {
+            callback(new Error('server error'))
+        }
     })
 
-    xhr.open('GET', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+    // const xhr = new XMLHttpRequest
 
-    xhr.setRequestHeader('Authorization', `Bearer ${token}`)
+    // xhr.addEventListener('load', event => {
 
-    xhr.send() 
+    //     const status = event.target.status
+
+        
+    //     if (status === 200) {
+            
+    //         const json = event.target.responseText
+    //         const data = JSON.parse(json)
+
+    //         const { notes = [] } = data
+            
+    //         if (noteId) {
+                
+    //             const index = notes.findIndex(note => note.id === noteId)
+    //             notes.splice(index, 1)
+
+    //             {
+    //                 const xhr = new XMLHttpRequest
+    
+    //                 xhr.addEventListener('load', event => {
+    
+    //                     const status = event.target.status
+    
+    //                     if (status === 204) {
+    //                         callback(null)
+    //                     } else if (status >= 400 && status < 500) {
+    //                         const json = event.target.responseText
+    
+    //                         const data = JSON.parse(json)
+    
+    //                         callback(new Error(data.error))
+    //                     } else callback(new Error('server error'))
+    //                 })
+    
+    //                 xhr.open('PATCH', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+    
+    //                 xhr.setRequestHeader('Authorization', `Bearer ${token}`)
+    //                 xhr.setRequestHeader('Content-Type', 'application/json')
+    
+    //                 const data = { notes }
+    
+    //                 const json = JSON.stringify(data)
+    
+    //                 xhr.send(json)
+    
+    //             }
+    //         }
+    //     } else if (status >= 400 && status < 500) {
+    //         const json = event.target.responseText
+
+    //         const data = JSON.parse(json)
+
+    //         callback(new Error(data.error))
+    //     } else callback(new Error('server error'))
+    // })
+
+    // xhr.open('GET', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+
+    // xhr.setRequestHeader('Authorization', `Bearer ${token}`)
+
+    // xhr.send() 
 }
+
+
+// 
