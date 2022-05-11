1b6b7688e9099220da178486c6c19896acdd1f4e
Natalia Garcia <nagarcia.docente@gmail.com>
2022-05-11 09:43:21 +0200

    add the reference sticker-app 3

---
+++ staff/nath-garcia/sticker.3/src/components/Home.jsx
@@ -0,0 +1,60 @@
+const { Component } = React
+
+class Home extends Component {
+    state = { name: null, timestamp: null, view: 'list' }
+
+    handleLogoutClick = () => {
+        delete sessionStorage.username
+
+        this.props.onUserLoggedOut()
+    }
+
+    componentDidMount() {
+        retrieveUser(sessionStorage.username, (error, user) => {
+            if (error) {
+                alert(error.message)
+
+                return
+            }
+
+            this.setState({ name: user.name })
+        })
+    }
+
+    handleAddClick = () => {
+        saveNote(sessionStorage.username, null, null, error => {
+            if (error) {
+                alert(error.message)
+
+                return
+            }
+
+            this.setState({ timestamp: Date.now() })
+        })
+    }
+
+    handleProfileClick = () => this.setState({ view: 'profile' })
+
+    handleHomeClick = () => this.setState({ view: 'list' })
+
+    render() {
+        return <div className="Home Container">
+            <header className="Home__header Container Container--row Container--spread-sides">
+                <button className="Button Button--no-border Home__home" onClick={this.handleHomeClick}>📋</button>
+                <div>
+                    <button className="Button Button--no-border Home__profile" onClick={this.handleProfileClick}>{this.state.name}</button>
+                    <button className="Button Button--no-border Home__logout" onClick={this.handleLogoutClick}>Logout</button>
+                </div>
+            </header>
+
+            <main className="Home__body Container">
+                {this.state.view === 'list' && <StickerList timestamp={this.state.timestamp} />}
+                {this.state.view === 'profile' && <Profile />}
+            </main>
+
+            <footer className="Home__footer Container">
+                <button className="Home__addSticker" onClick={this.handleAddClick}>+</button>
+            </footer>
+        </div>
+    }
+}
---
+++ staff/nath-garcia/sticker.3/src/logic/saveNote.js
@@ -0,0 +1,28 @@
+function saveNote(username, noteId, text, callback) {
+    const userExists = db.users.some(user => user.username === username)
+
+    if (!userExists) {
+        callback(new Error(`username "${username}" does not exist`))
+
+        return
+    }
+
+    let note = db.notes.find(note => note.id === noteId)
+
+    if (!note) {
+        note = new Note(username, text)
+        note.id = noteId
+
+        db.notes.push(note)
+    } else {
+        if (note.username !== username) {
+            callback(new Error(`user "${username}" does not own note with id "${noteId}"`))
+
+            return
+        }
+
+        note.text = text
+    }
+
+    callback(null)
+}
