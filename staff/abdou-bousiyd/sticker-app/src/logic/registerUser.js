fb2a0fde0f7362c2fdcda5056104138b636d0039
abousiyd <abdou.bousiyd@gmail.com>
2022-05-12 09:36:40 +0200

    add Apium to the register logic

--- staff/abdou-bousiyd/sticker-app/src/logic/registerUser.js
+++ staff/abdou-bousiyd/sticker-app/src/logic/registerUser.js
@@ -1,32 +1,59 @@
-function registerUser(name, username, password, callback) {
+// function registerUser(name, username, password, callback) {
 
-    if(typeof name !== 'string') throw new TypeError(`name ${name} is not a string`)
+//     if(typeof name !== 'string') throw new TypeError(`name ${name} is not a string`)
 
     
-    const xhr = new XMLHttpRequest
+//     const xhr = new XMLHttpRequest
+//     const url = 'https://b00tc4mp.herokuapp.com/api/v2/users'
+
+//     xhr.addEventListener('load', event => {
+//         const status = event.target.status
+
+//         if(status === 201)
+//             callback(null)
+//         else if (status >= 400 && status < 500) {
+//             const json = event.target.responseText
+
+//             const data = JSON.parse(json)
+
+//             callback(new Error(data.error))
+//         }  else callback(new Error('server error'))
+
+//     })
+//     xhr.open('POST', url)
+
+//     xhr.setRequestHeader('Content-Type', 'application/json')
+
+//     const data = { name, username, password }
+
+//     const json = JSON.stringify(data)
+
+//     xhr.send(json)
+// }
+
+
+function registerUser(name, username, password, callback) {
+
+    const api = new Apium
+
     const url = 'https://b00tc4mp.herokuapp.com/api/v2/users'
 
-    xhr.addEventListener('load', event => {
-        const status = event.target.status
-
-        if(status === 201)
+    api.call('POST', url, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, username, password })
+    }, (error, {status}) => {
+        if(error) {
+            callback(error)
+            return
+        }
+        if(status === 201) {
             callback(null)
-        else if (status >= 400 && status < 500) {
-            const json = event.target.responseText
-
-            const data = JSON.parse(json)
-
-            callback(new Error(data.error))
-        }  else callback(new Error('server error'))
-
+        }else if (status >= 400 && status < 500) {       
+                const data = JSON.parse(json)
+    
+                callback(new Error(data.error))
+            }  else callback(new Error('server error'))
     })
-    xhr.open('POST', url)
-
-    xhr.setRequestHeader('Content-Type', 'application/json')
-
-    const data = { name, username, password }
-
-    const json = JSON.stringify(data)
-
-    xhr.send(json)
 }
