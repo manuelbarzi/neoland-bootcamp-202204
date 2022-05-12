08dd079d6e80ee7f54dfcc0f3cfab9b4000ba41e
abousiyd <abdou.bousiyd@gmail.com>
2022-05-12 14:58:37 +0200

    add Apium to logic retriveNotes

--- staff/abdou-bousiyd/sticker-app/src/logic/retrieveNotes.js
+++ staff/abdou-bousiyd/sticker-app/src/logic/retrieveNotes.js
@@ -1,23 +1,54 @@
 function retrieveNotes(token, callback) {
-    const xhr = new XMLHttpRequest
-    const url = 'https://b00tc4mp.herokuapp.com/api/v2/users'
-    
-    xhr.addEventListener('load', event => {
-        const status = event.target.status 
 
+    const api = new Apium
+    const url = 'https://b00tc4mp.herokuapp.com/api/v2/users'
+
+    api.call('GET', url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ token })
+    }, (error, {status, payload}) => {
+        if(error) return callback(error)
         if(status === 200) {
-            const json = event.target.responseText
-            const data = JSON.parse(json)
-            callback(null, data.notes)
-        }else if(status >= 400 && status < 500) {
-            const json = event.target.responseText
-            const data = JSON.parse(json)
+            const data = JSON.parse(payload)
+            const {notes} = data
+            callback(null, notes)
+        }else if (status >= 400 && status < 500) {
+            logger.warn('response - client error status ' + status)
+
+            const data = JSON.parse(payload)
+
             callback(new Error(data.error))
+        } else {
+            logger.error('response - server error status ' + status)
+
+            callback(new Error('server error'))
         }
     })
-
-    xhr.open('GET', url)
-    xhr.setRequestHeader('Authorization', `Bearer ${token}`)
-    xhr.send()
-
 }
+
+
+// function retrieveNotes(token, callback) {
+//     const xhr = new XMLHttpRequest
+//     const url = 'https://b00tc4mp.herokuapp.com/api/v2/users'
+    
+//     xhr.addEventListener('load', event => {
+//         const status = event.target.status 
+
+//         if(status === 200) {
+//             const json = event.target.responseText
+//             const data = JSON.parse(json)
+//             callback(null, data.notes)
+//         }else if(status >= 400 && status < 500) {
+//             const json = event.target.responseText
+//             const data = JSON.parse(json)
+//             callback(new Error(data.error))
+//         }
+//     })
+
+//     xhr.open('GET', url)
+//     xhr.setRequestHeader('Authorization', `Bearer ${token}`)
+//     xhr.send()
+// }
+
