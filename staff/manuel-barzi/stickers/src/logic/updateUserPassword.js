b191257181aeea14c671a6ba1a42b15ef0a3ba42
manuelbarzi <manuelbarzi@gmail.com>
2022-05-13 10:39:45 +0200

    add custom errors and validators

--- staff/manuel-barzi/stickers/src/logic/updateUserPassword.js
+++ staff/manuel-barzi/stickers/src/logic/updateUserPassword.js
@@ -1,31 +1,11 @@
-function updateUserPassword(username, password, newPassword, newPasswordRepeat, callback) {
-    const user = db.users.find(user => user.username === username)
+function updateUserPassword(token, password, newPassword, newPasswordRepeat, callback) {
+    validateJwt(token)
+    validatePassword(password)
+    validatePassword(newPassword, 'new password')
+    validatePassword(newPasswordRepeat, 'new password repeat')
 
-    if (!user) {
-        callback(new Error(`user with username "${username}" does not exist`))
 
-        return
-    }
+    if (newPassword !== newPasswordRepeat)
+        throw new Error('new password and new password repeat are not the same')
 
-    if (user.password !== password) {
-        callback(new Error('wrong password'))
-
-        return
-    }
-
-    if (password === newPassword) {
-        callback(new Error('current password and new password are the same'))
-
-        return
-    }
-
-    if (newPassword !== newPasswordRepeat) {
-        callback(new Error('new password and new password repeat are not the same'))
-
-        return
-    }
-
-    user.password = newPassword
-
-    callback(null)
 }
