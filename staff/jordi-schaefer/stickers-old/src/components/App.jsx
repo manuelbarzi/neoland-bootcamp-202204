c4d890361cb00a4c50717c3ac98a86db48246c65
JSchaefer21 <jschaefer21@gmail.com>
2022-05-16 22:56:01 +0200

    change folder name

---
+++ staff/jordi-schaefer/stickers-old/src/components/App.jsx
@@ -0,0 +1,21 @@
+const { Component } = React
+
+class App extends Component {
+    state = { view: sessionStorage.username? 'home' : 'login' }
+
+    handleUserRegistered = () => this.setState({ view: 'login' })
+    handleUserLoggedIn = () => this.setState({ view: 'home' })
+
+    handleRegisterNavigation = () => this.setState({ view: 'register' })
+    handleLoginNavigation = () => this.setState({ view: 'login' })
+
+    handleUserLoggedOut = () => this.setState({ view: 'login' })
+
+    render() {
+        return <div className="App">
+            {this.state.view === 'login' && <Login onUserLoggedIn={this.handleUserLoggedIn} onRegisterLinkClicked={this.handleRegisterNavigation} />}
+            {this.state.view === 'register' && <Register onUserRegistered={this.handleUserRegistered} onLoginLinkClicked={this.handleLoginNavigation} />}
+            {this.state.view === 'home' && <Home onUserLoggedOut={this.handleUserLoggedOut} />}
+        </div>
+    }
+}
