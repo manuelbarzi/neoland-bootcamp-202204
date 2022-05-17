190d4347e261f7cc99f92678c33919f1b8dcaf45
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-05-17 12:44:17 +0200

    start race

---
+++ staff/federico-lopez/car-race.js
@@ -0,0 +1,12 @@
+/*
+3 cars
+first car that reaches 100 steps (count) wins
+take into account two or all cars can reach at the same time (tie)
+show results below
+*/
+
+const thief = '🚘'
+const police = '🚔'
+const taxi = '🚖'
+const bus = '🚍'
+
