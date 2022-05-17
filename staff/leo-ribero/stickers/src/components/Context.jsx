0f33cf459db06cd1f6defe3ef29a3d53735935b0
leoribero <leo@ribero.net>
2022-05-17 07:15:47 +0200

    before erasing wrong stickers_node files structure s7d1 afternoon

---
+++ staff/leo-ribero/stickers/src/components/Context.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const { createContext } = React
+
+import { createContext } from 'react'
+
+const Context = createContext()
+
+export default Context
---
+++ staff/leo-ribero/stickers/src/logic/retrieveNotes.js
@@ -0,0 +1,39 @@
+function retrieveNotes(token, callback) {
+    const logger = new Logger('retrieveNotes')
+
+    logger.info('call')
+
+    // TODO validate input args
+
+    const api = new Apium('https://b00tc4mp.herokuapp.com/api')
+
+    logger.info('request')
+
+    api.get('v2/users', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }, (error, response) => {
+        if (error) return callback(error)
+
+        logger.info('response')
+
+        const { status, payload } = response
+
+        if (status >= 400 && status < 500) {
+            const data = JSON.parse(payload)
+
+            callback(new Error(data.error))
+        } else if (status >= 500)
+            callback(new Error('server error'))
+        else if (status === 200) {
+            const data = JSON.parse(payload)
+
+            let { notes = [] } = data 
+
+            notes = notes.map(note => new Note(note.id, note.text, new Date(note.date)))
+
+            callback(null, notes)
+        }
+    })
+}
