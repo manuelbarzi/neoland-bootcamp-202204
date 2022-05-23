dc47f279a9722c791ffbe593adc1ce50f2c84faa
Natalia Garcia <nagarcia.docente@gmail.com>
2022-05-23 09:39:08 +0200

    add more off notes

--- staff/nath-garcia/notes/src/utils/index.js
+++ staff/nath-garcia/notes/src/utils/index.js
@@ -1,7 +1,11 @@
 const { createCustomError } = require('./createCustomError')
 const createId = require('./createId')
+const { parseCookies } = require('./parseCookies')
+const { deleteFiles } = require('./deleteFiles')
 
 module.exports = {
     createCustomError,
-    createId
+    createId,
+    parseCookies,
+    deleteFiles
 }
