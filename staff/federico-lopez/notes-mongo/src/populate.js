a9224f96d2884a00e466843ba9802d9b1344cf06
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-05-23 23:38:37 +0200

    CRUD en mongo

--- staff/federico-lopez/notes-mongo/src/populate.js
+++ staff/federico-lopez/notes-mongo/src/populate.js
@@ -1,48 +1,63 @@
-// const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb')
 
-// MongoClient.connect('mongodb://localhost:27017', (error, connection) => {
-//     if (error) return console.error(error)
+MongoClient.connect('mongodb://localhost:27017', (error, connection) => {
+    if (error) return console.error(error)
 
-//     const db = connection.db('notes-db')
+    const db = connection.db('notes-db')
 
-//     const users = db.collection('users')
-//     const notes = db.collection('notes')
+    const users = db.collection('users')
+    const notes = db.collection('notes')
 
-//     users.deleteMany({}, (error) => {
-//         if (error) return console.error(error)
+    users.deleteMany({}, (error) => {
+        if (error) return console.error(error)
 
-//         users.insertMany([
-//             { name: 'Peter Pan', username: 'peter', password: '123123123' },
-//             { name: 'Charly Pan', username: 'charly', password: '123123123' },
-//             { name: 'Charly Smith', username: 'charlysmith', password: '123123123' }
-//         ],
-//             (error, result) => {
-//                 if (error) return console.error(error)
+        notes.deleteMany({}, error => {
+            if (error) return console.error(error)
 
-//                 console.log(result)
-//                 const { insertedIds } = result
+        users.insertMany([
+            { name: 'Peter Pan', username: 'peter', password: '123123123' },
+            { name: 'Charly Pan', username: 'charly', password: '123123123' },
+            { name: 'Charly Smith', username: 'charlysmith', password: '123123123' }
+        ], (error, result) => {
+                if (error) return console.error(error)
 
-//                 let _error
+                console.log(result)
+                const { insertedIds } = result
 
-//                 for (const key in insertedIds) {
-//                     if (!_error) {
-//                         notes.insertMany([
-//                             { user: insertedIds[key].toString(), text: `soy la nota 1`, date: new Date },
-//                             { user: insertedIds[key].toString(), text: `soy la nota 2`, date: new Date },
-//                             { user: insertedIds[key].toString(), text: `soy la nota 3`, date: new Date }
-//                         ],
-//                             (error, result) => {
-//                                 if (error) console.error(_error = error)
+                let _error, count = 0
 
-//                                 console.log(result)
-//                             }
-//                         )
-//                     }
-//                 }
-//             }
-//         )
-//     })
-// })
+                for (const key in insertedIds) {
+                    if (!_error) {
+                        notes.insertMany([
+                            { user: insertedIds[key].toString(), text: `soy la nota 1`, date: new Date },
+                            { user: insertedIds[key].toString(), text: `soy la nota 2`, date: new Date },
+                            { user: insertedIds[key].toString(), text: `soy la nota 3`, date: new Date }
+                        ], (error, result) => {
+                                if (error) return console.error(_error = error)
+
+                                console.log(result)
+
+                                count++
+                                if(count === Object.keys(insertedIds).length) {
+                                    notes.find({ text: /.*3.*/ }).toArray((error, result) => {
+                                        console.log(result)
+
+                                    notes.updateOne({ user: insertedIds[2].toString(), text: /.*2.*/ }, { $set : { text: 'yo era la nota 2, ahora soy la 4' } }, (error, result) => {                                        
+                                        console.log(result)
+
+                                        notes.updateMany({ user: insertedIds[0].toString() }, { $set : { text: 'nosotras somos las notas de Peter Pan' } }, (error, result) => {
+                                            console.log(result)
+                                        })
+                                    })
+                                })
+                            }
+                        })
+                    }
+                }
+            })
+        })
+    })
+})
 
 
 
@@ -76,43 +91,42 @@
 // })
 
 
-const { MongoClient } = require('mongodb')
+// const { MongoClient } = require('mongodb')
 
 
-MongoClient.connect('mongodb://localhost:27017', (error, connection) => {
+// MongoClient.connect('mongodb://localhost:27017', (error, connection) => {
 
-    if (error) throw error
+//     if (error) throw error
 
-    const db = connection.db('notes-db') // use notes-db
+//     const db = connection.db('notes-db') // use notes-db
 
-    const users = db.collection('users') // db.users
-    const notes = db.collection('notes') // db.notes
+//     const users = db.collection('users') // db.users
+//     const notes = db.collection('notes') // db.notes
 
-    users.deleteMany({}, (error, result) => { // db.users.deleteMany({})
+//     users.deleteMany({}, (error, result) => { // db.users.deleteMany({})
 
-        if (error) throw error
+//         if (error) throw error
 
-        console.table(result)
+//         console.table(result)
 
-        users.insertOne({
-            name: 'John Doe',
-            username: 'johndoe',
-            password: '123123123'
-        }, 
-        // db.users.insertOne({...})
-        (error, result) => {
-            if (error) throw error
+//         users.insertOne({
+//             name: 'John Doe',
+//             username: 'johndoe',
+//             password: '123123123'
+//         }, 
+//         (error, result) => {
+//             if (error) throw error
     
-            console.log(result)
+//             console.log(result)
                 
-            users.find({ name: 'John Doe'}).toArray((error, result) => {
-                // db.users.find({ "name": "John Doe" })
-                if (error) throw error
-                debugger
+//             users.find({ name: 'John Doe'}).toArray((error, result) => {
+//                 // db.users.find({ 'name': 'John Doe' })
+//                 if (error) throw error
+//                 debugger
         
-            })    
-        })
+//             })    
+//         })
     
 
-    })
-})
+//     })
+// })
