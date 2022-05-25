d99762129f9bcc557104cdfe643283223fa77358
Diegocchaves <diegocchaves21@gmail.com>
2022-05-25 16:20:48 +0200

    add notes.schemas

---
+++ staff/diego-carvalho/notes.schemas/src/logic/index.js
@@ -0,0 +1,11 @@
+const registerUser = require('./registerUser')
+const authenticateUser = require('./authenticateUser')
+const updateUser = require('./updateUser')
+const retrieveUser = require('./retrieveUser')
+
+module.exports = {
+    registerUser,
+    authenticateUser,
+    updateUser,
+    retrieveUser
+}
