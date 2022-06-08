f74b26127f437891e6d5ad5f105bc8cf86762628
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-06-08 20:52:24 +0200

    add

---
+++ staff/federico-lopez/proyecto-final/app/pages/create-interpretation.js
@@ -0,0 +1,13 @@
+import {  } from "../logic"
+
+export default function CreateInterpretation() {
+    
+
+
+    return <>
+        <form onSubmit={onFormSubmit}>
+            <input type="search" name="search" id="search"></input>
+            <button type="submit">SEARCH</button>
+        </form>
+    </>
+}
