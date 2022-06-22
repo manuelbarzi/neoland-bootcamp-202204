f21a1e483eceb65f4ff37e27be92f591ea784188
Jacint Blanch <jacintblanch5@gmail.com>
2022-06-22 15:27:18 +0200

    landing

--- staff/jacint-blanch/final-project/app-client/src/App.js
+++ staff/jacint-blanch/final-project/app-client/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Landing from "./components/Landing/Landing.jsx";
 import Register from "./components/Register/Register.jsx";
 import Login from "./components/Login/Login.jsx";
 import HeatMap from "./components/HeatMap/HeatMap.jsx";
@@ -7,6 +8,7 @@ import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 function App() {
   return (<div className="App Container">
         <Routes>
+          <Route path="/landing" element={<Landing />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/HeatMap" element={<HeatMap />} />
---
+++ staff/jacint-blanch/final-project/app-client/src/components/Landing/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Landing.css'
+import Navbar from './Navbar';
+import './Navbar.css';
+
+
+
+const Landing = () => {
+   
+    return(
+            <div className='App'>
+                <Navbar />
+            </div>
+    )
+}
+
+
+export default Landing
