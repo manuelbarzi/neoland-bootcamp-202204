989562ac74709b2e0af54e6d3616412fb4552639
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-06-29 12:43:31 +0200

    update checkSpotify session

--- staff/federico-lopez/proyecto-final/app/logic/checkSpotifySession.js
+++ staff/federico-lopez/proyecto-final/app/logic/checkSpotifySession.js
@@ -21,11 +21,13 @@ export async function checkSpotifySession(token, code) {
             }
         })
 
-    const data = JSON.parse(payload)
-
-    if (status === 200) return data.isSessionActive
-
-    else if (status >= 400 && status < 500) throw new Error(data.error)
-
-    else if (status >= 500) throw new Error('server error')
+        if (status === 200) {
+            const data = JSON.parse(payload)
+    
+            return data.isSessionActive
+        } else if (status >= 400 && status < 500) {
+            const data = JSON.parse(payload)
+    
+            throw new Error(data.error)
+        } else if (status >= 500) throw new Error('server error')
 }
